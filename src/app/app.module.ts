3771f403e3fa4aaf949724ab42d574183241df7b
sipoufo <sipoufoknj@gmail.com>
2024-01-23 09:52:01 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrintTemplateComponent } from './template/print-template/print-template.component';
+import { TableComponent } from './table/table.component';
+import { NgxPrintModule } from 'ngx-print';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrintTemplateComponent,
+    TableComponent
   ],
   imports: [
+    NgxPrintModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/template/print-template/print-template.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-print-template',
+  templateUrl: './print-template.component.html',
+  styleUrls: ['./print-template.component.scss']
+})
+export class PrintTemplateComponent {
+
+}
